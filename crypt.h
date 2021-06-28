95f3c76ac21a545351a12f26dcaa6e1e4664c91c
root <root@localhost.localdomain>
2021-06-28 17:19:57 +0800

    1. optimized MACROS 2. fix a bug in chown(will lead tar exit on error code) todo: there's still a strange bug not found yet. Occured when configure  qemu-6.0, a temp .o file is not complete

---
+++ crypt.h
@@ -0,0 +1,20 @@
+#ifdef __CRYPT_H__
+#define __CRYPT_H__
+
+#ifndef FILEBLOCK
+#define FILEBLOCK 1024
+#endif
+
+#ifndef ASE_KEYLEY
+#define AES_KEYLEN 128
+#endif
+
+#define AESBLOCK  (AES_KEYLEN/8)
+
+#ifndef PATH_MAX
+#define PAX_MAX 4096
+#endif
+
+
+
+#endif
