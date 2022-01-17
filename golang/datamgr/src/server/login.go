cc0dfbd35c4db2de7a0ca0227d294e85d24a9565
healthmeng <24625527@qq.com>
2022-01-17 17:23:31 +0800

    test login response

--- golang/datamgr/src/server/login.go
+++ golang/datamgr/src/server/login.go
@@ -2,17 +2,60 @@ package main
 
 import (
 	"net/http"
-/*	"fmt"
-	"os"
+	"encoding/json"
+	"fmt"
+/*	"os"
 	_"dbop"
 	*/
 )
 
 /*
-	
+{
+	"name":user_name,
+	"passwd":password,
+	"primask",0 // default
+}
 */
 
+type AuthInfo struct{
+	Name string `json:"name"`
+	Passwd string `json:"passwd"`
+	PriMask int32 `json:"primask"`
+}
+
+type TokenInfo struct{
+	Id int32 `json:"id"`
+	Token int64 `json:"token"`
+	Status int32 `json:"retval"`
+	ErrInfo	string `json:"errinfo"`
+}
+
+func NewToken()*TokenInfo{
+	token:=&TokenInfo{Id:-1,Token:-1,Status:-1,ErrInfo:"Error Parameter"}
+	return token
+}
+
+func (token *TokenInfo)GetUserInfo(info* AuthInfo)error{
+	// if return nil, means authoration succeeded,all fields should be fill correctly according to AuthInfo
+	return nil
+}
+
 func LoginFunc(w http.ResponseWriter, r *http.Request){
+	if r.Method=="POST"{
+		token:=NewToken()
+		w.Header().Set("Content-Type","application/json")
+		var ainfo AuthInfo
+		err:= json.NewDecoder(r.Body).Decode(&ainfo)
+		if err!=nil{
+			fmt.Println("Decode json error:",r.Body,"-",err)
+			return
+		}
+		token.GetUserInfo(&ainfo)
+		// check user/passwd
+	}else{
+//		http.Redirect(w,r,"/",http.StatusFound)
+		http.NotFound(w,r)
+	}
 }
 
 func GetUserFunc(w http.ResponseWriter, r *http.Request){
